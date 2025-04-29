312280cec286478965bbc8a950509658d551f77f
EnzoLeman <ezekielaugustino@gmail.com>
2025-04-29 12:58:08 +0300

    Add base URL path "/Todo-App"

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Todo-App",
   server: {
     port: 5173,
     // Add proxy if you need to avoid CORS issues during development
